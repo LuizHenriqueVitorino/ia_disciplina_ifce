1c4a60138d885d25afd6f9621d79f54313972149
LuizHenriqueVitorino <henriquefisicoquimico@gmail.com>
2023-08-25 08:43:41 -0300

    renata

--- Atividade02/questao01.py
+++ Atividade02/questao01.py
@@ -1,5 +1,6 @@
 '''
-Questão 1 - Dado 2 pontos em um espaço: A(10, 1, 3) e B(4, 5, 4). Implemente a distancia de manhattan entre estes dois pontos?
+    Questão 1 - Dado 2 pontos em um espaço: A(10, 1, 3) e B(4, 5, 4). 
+    Implemente a distancia de manhattan entre estes dois pontos?
 '''
 
 #   Vetores
@@ -7,6 +8,15 @@ A = [10, 1, 3]
 B = [4, 5, 4]
 
 def distanciaManhattan(V1, V2):
+    """
+        Função que calcula a distância entre dois vetores V1 e V2.
+        Estes vetores precisam ter entradas numéricas e devem ser do mesmo tamanho.
+        
+        :param V1: array
+        :param V2: array
+
+        :return distancia
+    """
     distancia = 0
     try:
         for a in range(len(V1)):
